d41dade7732260dc6acf1a42b8cc8bcb44f7d4de
Mate Velkey <mate_velkey@epam.com>
2020-06-24 18:50:00 +0200

    Task 5 - Result css fix.

--- src/components/stateful/Result.jsx
+++ src/components/stateful/Result.jsx
@@ -48,7 +48,7 @@ class Result extends PureComponent {
                         {
                             value => {
                                 return (
-                                    <div id="result-container-movie-types" className="col-lg-9">
+                                    <div id="result-container-movie-types" className="col-xl-11 col-lg-9 col-md-8 col-sm-8 col-xs-12">
                                         {
                                             value.genres.map((genre, index) => {
                                                 return <p key={index}>{genre.name}</p>
@@ -59,7 +59,7 @@ class Result extends PureComponent {
                             }
                         }
                     </AppContext.Consumer>
-                    <div className="col-lg-3">
+                    <div className="col-xl-1 col-lg-3 col-md-4 col-sm-8 col-xs-12">
                         <div id="result-container-movie-sort-by-types">
                             <p>SORT BY</p>
                             <select onChange={this.handleSortByTypeChange}>
