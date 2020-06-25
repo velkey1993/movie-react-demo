e2853e170a4e2a4033698bf47ddf21ea83558ed6
mdomonkos <v-mdomonkos@expedia.com>
2020-06-25 14:09:08 +0200

    use callback and memo to reduce re-render on results

--- src/components/stateful/App.jsx
+++ src/components/stateful/App.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useCallback, useMemo} from "react";
 import "./App.css";
 import Result from "./Result";
 import ErrorBoundary from "./ErrorBoundary";
@@ -6,7 +6,7 @@ import * as mockMovieService from "../../service/MockMovieService";
 import {useStateWithLocaleStorage} from "../../utils/Custom";
 import TopComponent from "../stateless/TopComponent";
 
-const genres = [
+const GENRES = [
     {name: "ALL", value: ["All"]},
     {name: "DOCUMENTARY", value: ["Documentary"]},
     {name: "COMEDY", value: ["Animated Comedy"]},
@@ -22,10 +22,30 @@ function App() {
     const [movies, setMovies] = useStateWithLocaleStorage("movies", () => mockMovieService.read());
     const [movieId, setMovieId] = useState();
 
+    const updateMovieCallback = useCallback(
+        (movie) => {
+            movie = mockMovieService.update(movie);
+            setMovies((movies) =>
+                movies.map((item) => (item.id === movie.id ? movie : item))
+            );
+        },
+        [setMovies]
+    );
+
+    const deleteMovieCallback = useCallback(
+        (id) => {
+            id = mockMovieService.remove(id);
+            setMovies((movies) => movies.filter((item) => item.id !== id));
+        },
+        [setMovies]
+    );
+
+    const genresContext = useMemo(() => ({ genres: GENRES }), []);
+
     return (
         <>
             <div id="container" className={"container"}>
-                <AppContext.Provider value={{genres: genres}}>
+                <AppContext.Provider value={genresContext}>
                     <ErrorBoundary>
                         <TopComponent
                             movie={movies.find(movie => movie.id === movieId)}
@@ -39,20 +59,8 @@ function App() {
                     <ErrorBoundary>
                         <Result
                             movies={movies}
-                            updateMovie={(movie) => {
-                                movie = mockMovieService.update(movie);
-                                setMovies((movies) =>
-                                    movies.map((item) =>
-                                        item.id === movie.id ? movie : item
-                                    )
-                                );
-                            }}
-                            deleteMovie={(id) => {
-                                id = mockMovieService.remove(id);
-                                setMovies((movies) =>
-                                    movies.filter((item) => item.id !== id)
-                                );
-                            }}
+                            updateMovie={updateMovieCallback}
+                            deleteMovie={deleteMovieCallback}
                             showMovieDetails={setMovieId}
                         />
                     </ErrorBoundary>
